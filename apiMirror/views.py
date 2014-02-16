d7f2d3f8c14ac4d68166492b857fba8d5fa23fb7
Igor Popov <ihptru@gmail.com>
2014-02-16 16:20:28 +0400

    ready project

---
+++ apiMirror/views.py
@@ -0,0 +1,23 @@
+import os
+from django.http import HttpResponseRedirect, StreamingHttpResponse
+from django.conf import settings
+from django.http import Http404
+
+def index(request):
+    return HttpResponseRedirect('https://github.com/ihptru/OpenRA-Content-Engine/wiki/Set-up-MapAPI-mirror')
+
+def API(request, arg):
+	path = settings.PATH_TO_MAPS
+	listmaps = os.listdir(path)
+	oramap = ""
+	for item in listmaps:
+		if arg in item:
+			served_name = item.split(arg + "_")[1]
+			if not path.endswith('/'):
+				path = path + '/'
+			oramap = path + item.strip()
+	if oramap == "":
+		raise Http404
+	response = StreamingHttpResponse(open(oramap), content_type='application/zip')
+	response['Content-Disposition'] = 'attachment; filename = "%s"' % served_name
+	return response
---
+++ openraMapAPI/settings.py
@@ -0,0 +1,78 @@
+"""
+Django settings for openraMapAPI project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^nh9%r9v^8ab$5p%5t^omm09p$*lzx((!k6c91(0wu24k42^jc'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'openraMapAPI.urls'
+
+WSGI_APPLICATION = 'openraMapAPI.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Path to a directory where all maps are fetched over RSYNC from a primary Map API server
+PATH_TO_MAPS = ''
